e9497d90669eb318aa0c98fd8ad2e509fb949b7c
Ravi Prakash <ravi.prakash@kiwitech.com>
2017-01-09 17:41:47 +0530

    Final of 9 jan office

--- MyPA/Form1.cs
+++ MyPA/Form1.cs
@@ -5,31 +5,82 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Speech.Synthesis;
 
 namespace MyPA
 {
+     
     public partial class Form1 : Form
     {
+        static internal bool _isSpeaking = false;
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            int i;
-
+           
             progressBar1.Minimum = 0;
-            progressBar1.Maximum = 200;
+            progressBar1.Maximum = 2000;
 
-            for (i = 0; i <= 200; i++)
+            SystemInformationSpeech();
+           // string sysInfo = SystemInformation();
+           //// Thread childThread = new Thread(CallToChildThread);
+           // SpeakText.Speak("Initializing the system. " + sysInfo);
+           // if (!_isSpeak)
+           // {
+           //     SpeakText.Speak("Initializing the system. " + sysInfo);
+           // }
+        }
+       
+        
+        private string SystemInformationSpeech()//Getting system info
+        {
+            StringBuilder StringBuilder1 = new StringBuilder(string.Empty);
+            try
             {
-                progressBar1.Value = i;
+                SpeakText.Speak("Initializing. Activating Biometric and face recognization process. Activation Succeed.");
+               
+                SpeakText.Speak("Currently loading system files from drive.");
+                progressBar1.Value = 10;
+                SpeakText.Speak(string.Format("Operation System:  {0}", Environment.OSVersion));
+                if (Environment.Is64BitOperatingSystem)
+                    SpeakText.Speak(string.Format("64 Bit Operating System."));
+                else
+                    SpeakText.Speak(string.Format("32 Bit Operating System."));
+                SpeakText.Speak(string.Format("SystemDirectory:  {0}", Environment.SystemDirectory));
+                SpeakText.Speak(string.Format("ProcessorCount:  {0}", Environment.ProcessorCount));
+                progressBar1.Value = 20;
+                SpeakText.Speak(string.Format("UserDomainName:  {0}", Environment.UserDomainName));
+                SpeakText.Speak(string.Format("UserName: {0}", Environment.UserName));
+                //Drives
+                SpeakText.Speak(string.Format("LogicalDrives "));
+                foreach (System.IO.DriveInfo DriveInfo1 in System.IO.DriveInfo.GetDrives())
+                {
+                    try
+                    {
+                        SpeakText.Speak(string.Format("Drive: {0} VolumeLabel: " +
+                          "{1}. DriveType: {2}. DriveFormat: {3}." +
+                          "TotalSize: {4}. AvailableFreeSpace: {5}",
+                          DriveInfo1.Name, DriveInfo1.VolumeLabel, DriveInfo1.DriveType,
+                          DriveInfo1.DriveFormat, DriveInfo1.TotalSize, DriveInfo1.AvailableFreeSpace));
+                    }
+                    catch
+                    {
+                    }
+                }
+                SpeakText.Speak(string.Format("SystemPageSize:  {0}\n", Environment.SystemPageSize));
+                SpeakText.Speak(string.Format("Version:  {0}", Environment.Version));
             }
-
-            string sysInfo = SystemInformation();
+            catch
+            {
+            }
+            return StringBuilder1.ToString();
         }
 
         private string SystemInformation()//Getting system info
---
+++ MyPA/SpeakText.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+using System.Runtime.InteropServices;
+
+namespace MyPA
+{
+    class SpeakText
+    {
+        //SpeakText()
+        //{
+        //   bool _isCurrentlySpeaking = true;
+        //}
+        public static void Speak(string text)
+        {
+
+            Form1._isSpeaking = true;
+            SpeechSynthesizer reader = new SpeechSynthesizer();
+            PromptBuilder pb = new PromptBuilder();
+            reader.SelectVoice("eSpeak-ryan");
+            reader.Rate = 1;
+            reader.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Senior);
+            Console.Write(text);
+            pb.AppendText(text);
+            reader.Speak(pb);
+            Form1._isSpeaking = false;
+            //reader.SpeakCompleted += (x, y) =>
+            //{
+            //    Form1._isSpeak = false;
+            //};
+            // reader.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(reader_SpeakCompleted);
+        }
+        //private void reader_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        //{
+        //    reader.Dispose();
+        //}
+
+    }
+}
