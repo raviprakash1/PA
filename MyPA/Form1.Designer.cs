c691cff34a486a61c71f154bc2f8edbb491dea66
Ravi Prakash <ravi.prakash@kiwitech.com>
2017-01-09 13:31:04 +0530

    first commit

---
+++ MyPA/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace MyPA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(241, 266);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(412, 12);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(206, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Initializing...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(862, 430);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MyPA/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyPA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int i;
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 200;
+
+            for (i = 0; i <= 200; i++)
+            {
+                progressBar1.Value = i;
+            }
+
+            string sysInfo = SystemInformation();
+        }
+
+        private string SystemInformation()//Getting system info
+        {
+            StringBuilder StringBuilder1 = new StringBuilder(string.Empty);
+            try
+            {
+                StringBuilder1.AppendFormat("Operation System:  {0}\n", Environment.OSVersion);
+                if (Environment.Is64BitOperatingSystem)
+                    StringBuilder1.AppendFormat("\t\t  64 Bit Operating System\n");
+                else
+                    StringBuilder1.AppendFormat("\t\t  32 Bit Operating System\n");
+                StringBuilder1.AppendFormat("SystemDirectory:  {0}\n", Environment.SystemDirectory);
+                StringBuilder1.AppendFormat("ProcessorCount:  {0}\n", Environment.ProcessorCount);
+                StringBuilder1.AppendFormat("UserDomainName:  {0}\n", Environment.UserDomainName);
+                StringBuilder1.AppendFormat("UserName: {0}\n", Environment.UserName);
+                //Drives
+                StringBuilder1.AppendFormat("LogicalDrives:\n");
+                foreach (System.IO.DriveInfo DriveInfo1 in System.IO.DriveInfo.GetDrives())
+                {
+                    try
+                    {
+                        StringBuilder1.AppendFormat("\t Drive: {0}\n\t\t VolumeLabel: " +
+                          "{1}\n\t\t DriveType: {2}\n\t\t DriveFormat: {3}\n\t\t " +
+                          "TotalSize: {4}\n\t\t AvailableFreeSpace: {5}\n",
+                          DriveInfo1.Name, DriveInfo1.VolumeLabel, DriveInfo1.DriveType,
+                          DriveInfo1.DriveFormat, DriveInfo1.TotalSize, DriveInfo1.AvailableFreeSpace);
+                    }
+                    catch
+                    {
+                    }
+                }
+                StringBuilder1.AppendFormat("SystemPageSize:  {0}\n", Environment.SystemPageSize);
+                StringBuilder1.AppendFormat("Version:  {0}", Environment.Version);
+            }
+            catch
+            {
+            }
+            return StringBuilder1.ToString();
+        }
+    }
+}
